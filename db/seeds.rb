efa15494b889193c2d01c5395849f4f6ab250207
Jakob Bethmann <jakob.bethmann@gmail.com>
2018-01-29 18:10:38 +0100

    Frontend for home and index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,10 @@ puts "Seeding starts"
 names = ["Krawatte", "Schal", "Hosentraeger", "Einstecktuch", "Socken", "Fliege", "Guertel", "Manschettenknoepfe"]
 Item.destroy_all
 
-names.each do
+names.each do |name|
   i = 1
   4.times do
-    Item.create(name: "#{names.sample}#{i}", price: rand(20..400))
+    Item.create(name: "#{name}#{i}", price: rand(20..400))
     i += 1
   end
 end
