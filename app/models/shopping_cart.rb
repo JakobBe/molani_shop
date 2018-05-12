021bf19fe5cac0b5d2ffbd92f7a28272a1b995b1
Jakob Bethmann <jakob.bethmann@gmail.com>
2018-05-12 18:19:18 +0200

    minor changes

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -2,4 +2,16 @@ class ShoppingCart < ApplicationRecord
   belongs_to :user
   has_many :cart_items
   has_many :items, through: :cart_items
+
+  def shoppincart_list(cart_items)
+    list = []
+    cart_items.each do |item|
+      if list[].include? item
+        item.count += 1
+      else
+        list << item
+      end
+    end
+    return list
+  end
 end
